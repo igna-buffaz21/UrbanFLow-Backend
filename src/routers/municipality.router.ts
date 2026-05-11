de7e7cd88310619d8953c37d5fc9e1a0ae1d6f6a
AguLuque <agustinluque.lbz@gmail.com>
2026-05-11 15:45:32 -0300

    feat: agregue POST /municipalities con validación de nombre duplicado

--- src/routers/municipality.router.ts
+++ src/routers/municipality.router.ts
@@ -4,5 +4,7 @@ import { MunicipalityController } from "../controllers/municipality.controller";
 const router = Router();
 
 router.get("/", MunicipalityController.getMunicipalities);
+router.get("/:id", MunicipalityController.getMunicipalityById);
+router.post("/", MunicipalityController.createMunicipality);
 
 export default router;
