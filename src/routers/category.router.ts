adec64fbab9b30a5b224437c848c167d40bd01b3
AguLuque <agustinluque.lbz@gmail.com>
2026-05-19 14:11:15 -0300

    actualice mi rama con cambios de mis compañeros

--- src/routers/category.router.ts
+++ src/routers/category.router.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { CategoryController } from "../controllers/category.controller";
+import { requireAuth } from "../middlewares/auth.middleware";
 
 const router = Router();
 
-router.get("/", CategoryController.getCategories);
-router.post("/", CategoryController.createCategory);
+router.get("/", requireAuth, CategoryController.getCategories);
+router.post("/", requireAuth, CategoryController.createCategory);
 
 export default router;
